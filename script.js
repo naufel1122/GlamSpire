20b10b0949fb18830df700b02b93deb44316f85a
Muhammad Nofil <nofil.khan037@gmail.com>
2023-07-25 16:14:25 +0500

    working on products page

--- script.js
+++ script.js
@@ -10,7 +10,23 @@ window.addEventListener('scroll', isScrolling);
 // Pro loader======
 
 
-setTimeout(function(){
+setTimeout(function () {
     $('.loader_bg').fadeToggle();
-  }, 1000);
+}, 1000);
 
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
